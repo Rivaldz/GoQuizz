a0df497211d0641fb128bc2a74ea93b664b433a9
AdmajaRois <rois.da.silva@gmail.com>
2019-10-15 00:25:18 +0700

    membuat bottom navigation

--- app/src/main/java/com/valdo/goquizz/MainActivity.java
+++ app/src/main/java/com/valdo/goquizz/MainActivity.java
@@ -1,13 +1,27 @@
 package com.valdo.goquizz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.valdo.goquizz.fragments.CreateQuizFragment;
+import com.valdo.goquizz.fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {}
 }
--- app/src/main/java/com/valdo/goquizz/activities/SplashActivity.java
+++ app/src/main/java/com/valdo/goquizz/activities/SplashActivity.java
@@ -2,8 +2,11 @@ package com.valdo.goquizz.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.valdo.goquizz.MainActivity;
 import com.valdo.goquizz.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -12,5 +15,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        },5000);
+
     }
 }
