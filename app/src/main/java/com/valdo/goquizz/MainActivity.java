1298cde09974cb982bd474e916dc2e2d743f2ec2
AdmajaRois <rois.da.silva@gmail.com>
2019-10-15 17:42:53 +0700

    revisi bottom navigation

--- app/src/main/java/com/valdo/goquizz/MainActivity.java
+++ app/src/main/java/com/valdo/goquizz/MainActivity.java
@@ -5,23 +5,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.valdo.goquizz.activities.AddQuiz;
 import com.valdo.goquizz.fragments.CreateQuizFragment;
+import com.valdo.goquizz.fragments.EnterCodeFragment;
 import com.valdo.goquizz.fragments.HomeFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+    BottomNavigationView.OnNavigationItemSelectedListener{
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadFragment(new HomeFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {}
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()){
+            case R.id.action_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.action_play:
+                fragment = new EnterCodeFragment();
+                break;
+            case R.id.action_create:
+                Intent intent = new Intent(this, AddQuiz.class);
+                startActivity(intent);
+        }
+        return loadFragment(fragment);
+    }
 }
