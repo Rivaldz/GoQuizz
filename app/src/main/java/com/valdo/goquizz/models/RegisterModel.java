d85475de68dfb9bd56c4372fdef186b4a64195ae
Rivaldz <rivalzsetyo@gmail.com>
2019-10-23 13:18:54 +0700

    mengembalikan model yang di hapus

---
+++ app/src/main/java/com/valdo/goquizz/models/RegisterModel.java
@@ -0,0 +1,69 @@
+package com.valdo.goquizz.models;
+
+public class RegisterModel {
+    int key;
+    String nama;
+    String username;
+    String email;
+    String password;
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public RegisterModel() {
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "RegisterModel{" +
+                "nama='" + nama + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public RegisterModel(String nama, String username, String email, String password) {
+        this.nama = nama;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
