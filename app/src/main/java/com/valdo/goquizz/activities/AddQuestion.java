6d9ddec0d37b3f0fc00831ac97bc2d4c2c754f27
AdmajaRois <rois.da.silva@gmail.com>
2019-10-26 14:09:23 +0700

    adding action bar back button

--- app/src/main/java/com/valdo/goquizz/activities/AddQuestion.java
+++ app/src/main/java/com/valdo/goquizz/activities/AddQuestion.java
@@ -1,8 +1,10 @@
 package com.valdo.goquizz.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.valdo.goquizz.R;
 
@@ -12,5 +14,18 @@ public class AddQuestion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_question);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Add Question");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/valdo/goquizz/activities/AddQuiz.java
+++ app/src/main/java/com/valdo/goquizz/activities/AddQuiz.java
@@ -1,9 +1,11 @@
 package com.valdo.goquizz.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.valdo.goquizz.R;
@@ -14,6 +16,19 @@ public class AddQuiz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_quiz);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Add quiz");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+        }
     }
 
     public void addQuizHandler(View view) {
