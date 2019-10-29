9dd19a2ed53826483287a2236a57464299fcae2b
AdmajaRois <rois.da.silva@gmail.com>
2019-10-29 22:26:54 +0700

    adding button image

--- app/src/main/java/com/valdo/goquizz/activities/AddQuiz.java
+++ app/src/main/java/com/valdo/goquizz/activities/AddQuiz.java
@@ -1,18 +1,39 @@
 package com.valdo.goquizz.activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.strictmode.ImplicitDirectBootViolation;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.valdo.goquizz.R;
 
+import java.io.IOException;
+
+@RequiresApi(api = Build.VERSION_CODES.Q)
 public class AddQuiz extends AppCompatActivity {
+
+    private Button btnUpload;
+    private EditText title;
+    private EditText description;
+    private static final String TAG = AddQuiz.class.getCanonicalName();
+    private static final int GALLERY_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +41,17 @@ public class AddQuiz extends AppCompatActivity {
         setContentView(R.layout.activity_add_quiz);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Add quiz");
+        btnUpload = findViewById(R.id.button_add);
+        title = findViewById(R.id.input_title);
+        description = findViewById(R.id.input_description);
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), AddQuestion.class);
+                startActivity(intent);
+            }
+        });
     }
-
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
@@ -33,8 +63,29 @@ public class AddQuiz extends AppCompatActivity {
         }
     }
 
-    public void addQuizHandler(View view) {
-        Intent intent = new Intent(this, AddQuestion.class);
-        startActivity(intent);
+    public void handleChangeAvatar(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED){
+            return;
+        }
+        if (requestCode == GALLERY_REQUEST_CODE){
+            if (data != null){
+                try {
+                    Uri imageUri = data.getData();
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+
+                }catch (IOException e){
+                    Toast.makeText(this, "Tidak Bisa load gambar" , Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        }
     }
 }
