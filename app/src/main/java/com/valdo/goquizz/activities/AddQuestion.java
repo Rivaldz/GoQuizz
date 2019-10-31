7b647618a8ea1efa15ea57f5f80de73135f2b61c
Rivaldz <rivalzsetyo@gmail.com>
2019-10-31 18:51:34 +0700

    add cloud storage update

--- app/src/main/java/com/valdo/goquizz/activities/AddQuestion.java
+++ app/src/main/java/com/valdo/goquizz/activities/AddQuestion.java
@@ -4,15 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -20,14 +23,22 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.valdo.goquizz.R;
 import com.valdo.goquizz.models.AddQuestionModel;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Random;
+import java.util.UUID;
 
 import static android.text.TextUtils.isEmpty;
 
@@ -36,10 +47,17 @@ public class AddQuestion extends AppCompatActivity {
    private static final String TAG =  AddQuestion.class.getCanonicalName();
    private Button addQuestionBut;
    private ImageView avatarImage ;
+   private EditText inputQuestion, answer1, answer2, answer3, answer4;
+
    private DatabaseReference mFirebaseDatabase;
    private FirebaseDatabase mFirebaseInstance;
-   private EditText inputQuestion, answer1, answer2, answer3, answer4;
+   private StorageReference reference;
+
    private int userPin;
+   private static final int Image_Request_Code = 7;
+
+   private Bitmap imagae;
+   private Uri filePathUri;
 //   private String inputQuetionString, answer1String, answer2String, answer3String, answer4String;
    private static final int GALLERY_REQUEST_CODE = 1;
    private int i = 0;
@@ -55,6 +73,10 @@ public class AddQuestion extends AppCompatActivity {
         answer3 = findViewById(R.id.input_answer3);
         answer4 = findViewById(R.id.input_answer4);
         addQuestionBut = findViewById(R.id.buttonAddQuestion);
+        reference = FirebaseStorage.getInstance().getReference();
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
         mFirebaseInstance = FirebaseDatabase.getInstance();
 
         mFirebaseDatabase = mFirebaseInstance.getReference("BankSoal");
@@ -70,6 +92,7 @@ public class AddQuestion extends AppCompatActivity {
                    AddQuestionModel addQuestionModel = new AddQuestionModel(inputQuestion.getText().toString(), answer1.getText().toString(),answer2.getText().toString(),answer3.getText().toString()
                    ,answer4.getText().toString());
                    mFirebaseDatabase.child(String.valueOf(userPin)).child(inputQuestion.getText().toString()).setValue(addQuestionModel);
+                   uplodeImage();
                    Toast.makeText(getBaseContext(), "Berhasil menambahkan Soal",Toast.LENGTH_SHORT).show();
 
                }
@@ -80,7 +103,32 @@ public class AddQuestion extends AppCompatActivity {
             }
         });
 
+    }
 
+    private void uplodeImage(){
+       avatarImage.setDrawingCacheEnabled(true);
+       avatarImage.buildDrawingCache();
+       Bitmap bitmap = ((BitmapDrawable) avatarImage.getDrawable()).getBitmap();
+       ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+       bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+       byte[] bytes = stream.toByteArray();
+
+       String namaFile = UUID.randomUUID()+".jpg";
+       String pathImage = "gambar/"+namaFile;
+       UploadTask uploadTask = reference.child(pathImage).putBytes(bytes);
+       uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+           @Override
+           public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+               Toast.makeText(AddQuestion.this, "Berhasil menambahkan",Toast.LENGTH_SHORT).show();
+           }
+       })
+               .addOnFailureListener(new OnFailureListener() {
+                   @Override
+                   public void onFailure(@NonNull Exception e) {
+                       Toast.makeText(AddQuestion.this, "Gagal menambahkan",Toast.LENGTH_SHORT).show();
+                   }
+               });
     }
 
 
@@ -94,10 +142,12 @@ public class AddQuestion extends AppCompatActivity {
             if (data != null){
                 try {
                     final Uri imageUri = data.getData();
+                    filePathUri = imageUri;
                     final InputStream imageStream = getContentResolver().openInputStream(imageUri);
                     final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
                     final Bitmap bitmap  = Bitmap.createScaledBitmap(selectedImage, 1000, 500,false);
                     avatarImage.setImageBitmap(bitmap);
+                    this.imagae = bitmap;
 
                 }catch (IOException e){
                     Toast.makeText(this, "Gagal Mengamnbil gambar",Toast.LENGTH_LONG).show();
@@ -106,10 +156,30 @@ public class AddQuestion extends AppCompatActivity {
             }
         }
     }
+    public String getFileExtension(Uri uri){
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+
+        return  MimeTypeMap.getFileExtensionFromUrl(contentResolver.getType(uri));
+    }
 
     public void getImage(View view) {
         Intent inten = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(inten, GALLERY_REQUEST_CODE);
 
     }
+
+//    public void UplodeImageFileToFirebaseStorage(){
+//        if (filePathUri != null){
+//            StorageReference storageReference = databaseReference.child("All_image" + System.currentTimeMillis() + "." + getFileExtension(filePathUri));
+//            storageReference.putFile(filePathUri)
+//                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                        @Override
+//                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//
+//                        }
+//                    })
+//        }
+//
+//    }
 }
